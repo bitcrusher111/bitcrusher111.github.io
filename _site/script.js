01677484ac695f73350abf8677562b9300c490bb
bitcrusher111 <me@candidruetter.net>
2026-01-06 19:14:13 +0100

    new look

--- _site/script.js
+++ _site/script.js
@@ -1,40 +1,31 @@
-fetch('/navbar.html') // Access navbar.html from the root directory
-    .then(response => response.text())
-    .then(html => {
-        document.getElementById('navbar').innerHTML = html;
-
-        // Get the current page URL
-        let currentPage = window.location.href;
-
-        // Get all navigation links
-        let navLinks = document.querySelectorAll('#navbar a');
-
-        // Loop through each navigation link
-        navLinks.forEach(link => {
-            // Get the link's URL
-            let linkUrl = new URL(link.href);
-
-            // Add the active class to the link if it matches the current page URL or if it's a link to the root directory
-            if (linkUrl.href === currentPage || (linkUrl.pathname === '/' && currentPage.endsWith('index.js'))) {
-                link.classList.add('active');
-            }
-        });
-    })
-    .catch(error => {
-        console.error('Error fetching navigation bar:', error);
-    });
-
-
-
+// Navbar active link highlighting
 document.addEventListener('DOMContentLoaded', function() {
-const thumbnails = document.querySelectorAll('.thumbnails img');
-const mainImg = document.getElementById('mainImg');
+    // Get the current page URL
+    let currentPage = window.location.href;
 
-thumbnails.forEach(thumbnail => {
-    thumbnail.addEventListener('click', function() {
-    mainImg.src = this.src;
-    mainImg.alt = this.alt;
+    // Get all navigation links
+    let navLinks = document.querySelectorAll('.navbar a');
+
+    // Loop through each navigation link
+    navLinks.forEach(link => {
+        // Get the link's URL
+        let linkUrl = new URL(link.href);
+
+        // Add the active class to the link if it matches the current page URL
+        if (linkUrl.href === currentPage || (linkUrl.pathname === '/' && currentPage.endsWith('index.js'))) {
+            link.classList.add('active');
+        }
     });
-});
+
+    // Theater gallery thumbnail functionality
+    const thumbnails = document.querySelectorAll('.thumbnails img');
+    const mainImg = document.getElementById('mainImg');
+
+    thumbnails.forEach(thumbnail => {
+        thumbnail.addEventListener('click', function() {
+            mainImg.src = this.src;
+            mainImg.alt = this.alt;
+        });
+    });
 });
 
